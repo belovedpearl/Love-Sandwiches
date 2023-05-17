8a2c99b562911a95d31608d2adf4234c7e6a0aad
belovedpearl <>
2023-05-17 12:22:07 +0000

    Create get_sales_function to take data from user

--- run.py
+++ run.py
@@ -11,6 +11,20 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+'''
 sales = SHEET.worksheet('sales')
 data = sales.get_all_values()
 print(data)
+'''
+
+def get_sales_data():
+    '''
+    Get sales figure input from user
+    '''
+    print("Please enter sales from the last market")
+    print("Data should be six numbers")
+    print("Example: 10,20,30,40,50,60\n")
+    data_str = input("Enter your data here: ")
+    print(f"The data you entered is {data_str}")
+
+get_sales_data()
